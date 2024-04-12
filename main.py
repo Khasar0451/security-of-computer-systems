1b2b4db2ebdaf9a20bbfec25277482b664fa1342
Executed419 <lucker10@interia.pl>
2024-04-12 09:46:59 +0200

    more file types for sign_file, added key gen button

--- main.py
+++ main.py
@@ -16,15 +16,18 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Safety first")
         sign_file_button = QPushButton("Sign file")
         verify_button = QPushButton("Verify signature")
-        cryptic_button = QPushButton("Encryption/decryption ")
+        cryptic_button = QPushButton("Encryption/decryption")
+        key_button = QPushButton("Generate key")
         sign_file_button.clicked.connect(self.sign_file)
         verify_button.clicked.connect(self.verify)
         cryptic_button.clicked.connect(self.cryption)
+        key_button.clicked.connect(self.key_generation)
 
         hbox = QHBoxLayout()
         hbox.addWidget(sign_file_button)
         hbox.addWidget(verify_button)
         hbox.addWidget(cryptic_button)
+        hbox.addWidget(key_button)
 
         centralWidget = QWidget()
         centralWidget.setLayout(hbox)
@@ -33,16 +36,14 @@ class MainWindow(QMainWindow):
     def sign_file(self):
         file_dialog = QFileDialog()
         file_dialog.setFileMode(QFileDialog.FileMode.ExistingFile)  # max one file
-        file_dialog.setNameFilter("RSA files (*.rsa)")
-        file_dialog.exec()
+        file_dialog.setNameFilter("PDF files (*.pdf);;RSA files (*.rsa);;XML files (*.xml)")
 
         if file_dialog.exec() == QFileDialog.DialogCode.Rejected:
-            self.show_error("Error when choosing key")
+            self.show_error("Error when choosing key/files")
             return
         print(file_dialog.selectedFiles()[0].title())
 
     def verify(self):
-
         pass
 
     def cryption(self):
@@ -50,6 +51,15 @@ class MainWindow(QMainWindow):
             pin = self.insert_pin()
         except Exception as e:
             self.show_error("Invalid PIN")
+
+    def key_generation(self):
+        directory_dialog = QFileDialog()
+        directory_dialog.setFileMode(QFileDialog.FileMode.Directory)
+        if directory_dialog.exec():
+            directory = directory_dialog.selectedFiles()[0]
+            print(directory)
+        else:
+            self.show_error("Machine spirit is not satisfied with your lack of cooperation")
 
     def show_error(self, text):
         msg = QMessageBox()
@@ -61,9 +71,7 @@ class MainWindow(QMainWindow):
 
     def insert_pin(self):
         pin, ok = QInputDialog.getText(self, '"Encryption/decryption', 'Insert PIN?')
-
         # TODO validate PIN
-
         if ok:
             return pin
         else:
@@ -71,8 +79,6 @@ class MainWindow(QMainWindow):
 
 
 app = QApplication(sys.argv)
-
 window = MainWindow()
 window.show()
-
 app.exec()
