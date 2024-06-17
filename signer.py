7add542a32da2af3e3ee2bf97e914d81a3ec17e6
Executed419 <lucker10@interia.pl>
2024-06-17 22:13:30 +0200

    creating xml - error

--- signer.py
+++ signer.py
@@ -1,3 +1,4 @@
+import datetime
 import hashlib
 import cryptography.hazmat
 import xmlsig
@@ -5,8 +6,34 @@ from endesive import plain
 from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import padding
-import xades
+import xml.etree.ElementTree as ET
+import os
 
+
+def create_xml(path_with_file_name):
+    root = ET.Element('root')
+    size = ET.SubElement(root, "size")
+    size.text = os.stat(path_with_file_name).st_size
+
+    extension = ET.SubElement(root,"extension")
+    _, extension.text = os.path.splitext(path_with_file_name)
+
+    date_of_modification = ET.SubElement(root,"date_of_modification")
+    date_of_modification.text = os.path.getmtime(path_with_file_name) #m in getmtime as modified
+
+    user_name = ET.SubElement(root,"user_name")
+    user_name.text = "User A"
+
+    encrypted_hash = ET.SubElement(root, "encrypted_hash")
+    encrypted_hash.text = "TutajHash"
+
+    signature_timestamp = ET.SubElement(root, "signature_timestamp")
+    signature_timestamp.text = datetime.datetime.now()
+
+    tree = ET.ElementTree(root)
+    tree.write('a.xml')
+
+create_xml("R:\.Programowanie\info.txt")
 
 def generate_rsa():
     return cryptography.hazmat.primitives.asymmetric.rsa.generate_private_key(
